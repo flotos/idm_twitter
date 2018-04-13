feacdf985903f12aea89bb6ec1109bc96232af5a
Pol-Hervé Raillard <polherve.raillard@gmail.com>
2018-04-13 12:22:32 +0200

    adding front page (sample state)

---
+++ front/script.js
@@ -0,0 +1,29 @@
+google.charts.load('current', {
+  'packages':['geochart'],
+  // Note: you will need to get a mapsApiKey for your project.
+  // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+  'mapsApiKey': '	AIzaSyC0l6grkS_DX5RAY17ljAaE_-Gj76I1qdk'
+});
+google.charts.setOnLoadCallback(drawRegionsMap);
+
+function drawRegionsMap() {
+  var data = google.visualization.arrayToDataTable([
+    ['Country', 'Popularity'],
+    ['Germany', 0],
+    ['United States', 0.3],
+    ['Brazil', 0.4],
+    ['Canada', 0.5],
+    ['France', 0.6],
+    ['RU', 0.9]
+  ]);
+
+  var options = {
+    colorAxis: {minValue: 0, maxValue: 1, colors: ['#00B231' , 'white' , '#FF0003']},
+    backgroundColor: '#81d4fa',
+    datalessRegionColor: '#D9D9D9'
+  };
+
+  var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+  chart.draw(data, options);
+}
